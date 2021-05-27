c6c313a1bd4ed4394e3070177f8c3fdb6ae4b360
phichphichs <53659473+phichphichs@users.noreply.github.com>
2021-05-27 23:53:29 +0700

    init project

---
+++ pages/demo/demo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <a-input-number v-model="a"></a-input-number>
+    +
+    <a-input-number v-model="b"></a-input-number>
+    <a-button @click="calcSum">=</a-button>
+
+    {{ sum }}
+  </div>
+</template>
+
+<script>
+import { add } from '@/services/number'
+
+export default {
+  data: () => ({
+    a: 0,
+    b: 0,
+    sum: 0,
+  }),
+  created() {
+    this.a = 10
+    this.b = 20
+    this.calcSum()
+  },
+  methods: {
+    calcSum() {
+      this.sum = add(this.a, this.b)
+    },
+  },
+}
+</script>
+
+<style></style>

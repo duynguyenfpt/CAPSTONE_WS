bb6c0019704af535f2cb75645f6d74d11b7f0472
phichphichs <dinhlequynhhuong0805@gmail.com>
2021-06-08 02:27:23 +0700

    add and delete table

--- components/table-component/addTable.vue
+++ components/table-component/addTable.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
-    ADD TABLE
+    <b-row>
+      <b-col cols="4">
+        <label class="form-lab">Database Name</label>
+      </b-col>
+      <b-col>
+        <b-input
+          size="sm"
+          disabled
+          :placeholder="database.database_name"
+        ></b-input>
+      </b-col>
+    </b-row>
+    <b-row class="pt-2">
+      <b-col cols="4">
+        <label class="text-center">Table Name</label>
+      </b-col>
+      <b-col>
+        <b-form-input size="sm" v-model="table.table_name"></b-form-input>
+      </b-col>
+    </b-row>
+    <b-row class="text-center pt-3">
+      <b-col>
+        <b-button @click="addTable" variant="primary" size="sm"
+          >Add table</b-button
+        >
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -8,11 +34,13 @@
 import { addTable } from '@/service/table.service'
 
 export default {
+  props: {
+    database: {}
+  },
   data: () => ({
     loading: false,
     table: {
-      table_name: null,
-      database_infor_id: null
+      table_name: null
     }
   }),
 
@@ -20,15 +48,20 @@ export default {
     async addTable () {
       this.loading = true
       try {
-        const res = await addTable(this.table)
-        const tableID = res.table_id
-        if (tableID) {
-          // thong bao thanh cong
-        } else {
-          // thong bao loi
+        const body = {
+          table_name: this.table.table_name,
+          database_infor_id: this.database.id
         }
+        const res = await addTable(body)
+        const tableID = res.id
+        if (tableID) {
+          this.$message.success('Success!')
+        } else {
+          this.$message.error('Add table unsuccessfully')
+        }
+        this.$router.go()
       } catch (e) {
-        // thong bao loi
+        this.$message.error(e)
       } finally {
         this.loading = false
       }
@@ -37,6 +70,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>

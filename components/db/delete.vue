44a90265bc62d326a906be5323dfbe25444af58d
LinhCancer2306 <linhntt2306@gmail.com>
2021-06-09 00:28:25 +0700

    Update reload and popup confirm delete db

---
+++ components/db/delete.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-modal v-model="isVisible" title="Confirm" hide-footer>
+      <b-row>
+        <b-col>
+          <p>Do you want to delete this database?</p>
+        </b-col>
+      </b-row>
+      <b-row class="pt-3">
+        <b-col class="text-right">
+          <b-button size="sm" variant="danger" @click="onDeleteDB">
+            <b-spinner v-if="isLoading" variant="danger" small></b-spinner>Delete</b-button>
+          <b-button size="sm" variant="light" @click="onClose">
+            Cancel
+          </b-button>
+        </b-col>
+      </b-row>
+  </b-modal>
+</template>
+
+<script>
+import { deleteDatabaseDetail } from '@/service/db'
+export default {
+  data: () => ({
+    isVisible: false,
+    idItem: 0,
+    isLoading: false
+  }),
+  methods: {
+    async show (id) {
+      this.idItem = id
+      this.isVisible = true
+    },
+    onClose () {
+      this.isVisible = false
+    },
+    async onDeleteDB () {
+      try {
+        this.isLoading = true
+        const res = await deleteDatabaseDetail(this.idItem)
+        if (res.id) {
+          this.$message.error('Update unsuccessfully!')
+        } else {
+          this.$message.success('Success!')
+        }
+        this.getList()
+      } catch (e) {
+        this.$message.error(e)
+      } finally {
+        this.isLoading = false
+      }
+    }
+  }
+}
+</script>
+
+<style></style>

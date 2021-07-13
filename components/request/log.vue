e6bd2b1d002e75f1469e2f91ae828312c2fb46cf
LinhCancer2306 <linhntt2306@gmail.com>
2021-07-14 02:09:37 +0700

    Update request

--- components/request/log.vue
+++ components/request/log.vue
@@ -4,71 +4,74 @@
       <b-spinner variant="primary" label="Text Centered"></b-spinner>
     </div>
     <div v-else>
-      <div class="info">
-        <p><strong>Info!</strong> Some text...</p>
+      <div class="info" v-for="note in notes" :key="note.content">
+        <p><strong>Username: </strong> {{ note.content }}</p>
       </div>
-      <b-row class="pt-3">
-        <b-col class="text-right">
-          <b-button size="sm" variant="primary" @click="createLog" class="btn-add-request">
-            <b-spinner
-              v-if="isLoadingCreate"
+      <div class="create">
+        <b-row>
+          <b-col sm="12">
+            <b-form-textarea
+              id="textarea-small"
+              size="sm"
+              placeholder="Log Note"
+              v-model="note"
+            ></b-form-textarea>
+          </b-col>
+        </b-row>
+        <p class="msg-error" v-if="msg">{{ msg }}</p>
+        <b-row class="pt-3">
+          <b-col class="text-right">
+            <b-button
+              size="sm"
               variant="primary"
-              small
-            ></b-spinner
-            >Log</b-button
-          >
-          <b-button size="sm" variant="light" @click="onClose">
-            Cancel
-          </b-button>
-        </b-col>
-      </b-row>
+              @click="createLog"
+              class="btn-add-request"
+            >
+              <b-spinner
+                v-if="isLoadingCreate"
+                variant="primary"
+                small
+              ></b-spinner
+              >Log</b-button
+            >
+            <b-button size="sm" variant="light" @click="onClose">
+              Cancel
+            </b-button>
+          </b-col>
+        </b-row>
+      </div>
     </div>
   </b-modal>
 </template>
 
 <script>
-import { getDetailRequest, updateRequest } from '@/service/request'
-import { getListAccount } from '@/service/account'
+import { getLogByRequest, createLog } from '@/service/request'
 export default {
   data: () => ({
-    request: {
-      requestType: null,
-      status: null,
-      creatorId: null,
-      approvedById: null
-    },
-    opsStatus: [
-      { value: 'pending', text: 'Pending' },
-      { value: 'rejected', text: 'Rejected' },
-      { value: 'approved', text: 'Approved' }
-    ],
-    opsAccount: [
-      { value: '1', text: 'Huong' },
-      { value: '2', text: 'Linh' }
-    ],
+    notes: [],
     isLoading: false,
     isVisible: false,
-    isLoadingUpdate: false,
-    idItem: 0,
-    account: {
-      id: null
-    }
+    isLoadingCreate: false,
+    note: null,
+    msg: null
   }),
   methods: {
+    validateNote (value) {
+      if (this.note === null) {
+        this.msg = 'Please fill note before'
+      } else {
+        this.msg = ''
+      }
+    },
     async show (id) {
       this.idItem = id
       this.isVisible = true
       this.isLoading = true
+      this.note = null
+      this.msg = null
       try {
-        const res = await getDetailRequest(id)
-        this.request.requestType = res.data.requestType
-        this.request.status = res.data.status
-        // get username
-        const accounts = await getListAccount(1, 100)
-        this.opsAccount = accounts.data.map((acc) => ({
-          value: acc.id,
-          text: acc.username
-        }))
+        const res = await getLogByRequest(this.idItem)
+        this.notes = res.data
       } catch (e) {
         this.$notify({ type: 'error', text: e.message })
       } finally {
@@ -78,23 +81,21 @@ export default {
     onClose () {
       this.isVisible = false
     },
-    async updateRequest () {
-      try {
-        this.isLoadingUpdate = true
-        const body = {
-          status: this.request.status,
-          requestType: this.request.requestType,
-          approvedById: this.account.id
+    async createLog () {
+      this.validateNote(this.note)
+      if (this.msg === '') {
+        try {
+          const data = {
+            requestId: this.idItem,
+            content: this.note
+          }
+          createLog(data)
+          this.show(this.idItem)
+        } catch (e) {
+          this.$notify({ type: 'error', text: e.message })
+        } finally {
+          this.isLoadingCreate = false
         }
-        const data = await updateRequest(this.idItem, body)
-        if (data.id) throw new Error('Update failed')
-        this.$notify({ type: 'success', text: 'Update successful' })
-        this.$emit('onUpdated', data)
-      } catch (e) {
-        this.$notify({ type: 'error', text: e.message })
-      } finally {
-        this.isLoadingUpdate = false
-        this.isVisible = false
       }
     }
   }

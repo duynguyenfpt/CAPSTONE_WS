0cc5c9ef70f662c6257dcabc72b9b1d4d8797ac8
LinhCancer2306 <linhntt2306@gmail.com>
2021-08-19 20:54:08 +0700

    Create FE edit merge

---
+++ pages/request/_id/old.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Edit v-if="id" :id="id" />
+  </div>
+</template>
+
+<script>
+import Edit from '@/components/merge/edit2.vue'
+export default {
+  components: {
+    Edit
+  },
+  data: () => ({
+    id: null
+  }),
+  created () {
+    this.id = this.$route.params.id
+  }
+}
+</script>
+
+<style></style>
